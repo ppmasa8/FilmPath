d24d6ad4d5b8a8e2f57d7e341e072a186347679b
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-07 13:17:16 +0900

    view fix

---
+++ front/pages/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          color="indigo"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon dark>
+            mdi-plus
+          </v-icon>
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline red lighten-2">
+          Add movie on Lists.
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-spacer></v-spacer>
+        <Search_form></Search_form>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+import Search_form from "./Search_form";
+export default {
+  components: {Search_form}
+}
+</script>
---
+++ front/pages/Search_form.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="title"
+      :counter="40"
+      :rules="titleRules"
+      label="Title"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="status"
+      :items="statusItems"
+      :rules="[v => !!v || 'Status is required']"
+      label="Status"
+      required
+    ></v-select>
+
+    <v-select
+      v-model="rate"
+      :items="rateItems"
+      :rules="[v => !!v || 'Rating is required']"
+      label="Rating"
+      required
+    ></v-select>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    title: '',
+    titleRules: [
+      v => !!v || 'Title is required',
+      v => (v && v.length <= 40) || 'Title must be less than 40 characters',
+    ],
+    status: '',
+    statusItems: [
+      'Want to Watch',
+      'Watching now',
+      'Watched',
+    ],
+    rate: '',
+    rateItems: [
+      'Unwatch',
+      '1 star',
+      '2 star',
+      '3 star',
+      '4 star',
+      '5 star',
+    ],
+  }),
+
+  methods: {
+    validate () {
+      this.$refs.form.validate()
+    },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    },
+  },
+}
+</script>
