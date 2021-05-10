c81a7c1bb7b64980efa10a92ba6889d2edf04259
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-10 21:12:46 +0900

    add

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,30 +1,19 @@
 <template>
   <v-app id="inspire">
-    <Header></Header>
     <v-main>
       <v-container>
         <div>
           <Search></Search>
         </div>
 
-        <v-row>
-          <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
-          >
-            <v-card height="200">
-              <v-card-text
-                v-for="movie in movies"
-                :key="movie.id"
-              >
-                {{ movie.title }}
-                {{ movie.status }}
-                {{ movie.rate }}
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+        <v-data-table
+          :headers="headers"
+          :items="movies"
+          :items-per-page="5"
+          class="elevation-1"
+        >
+        </v-data-table>
+
       </v-container>
     </v-main>
   </v-app>
@@ -33,21 +22,31 @@
 <script>
 import Header from "./Header";
 import Search from "./Search";
-import axios from "axios";
 export default {
   components: {Search, Header},
 
   data: function() {
     return {
       movies: [],
+      headers: [
+        {
+          text: 'Title',
+          align: 'start',
+          sortable: false,
+          value: 'title',
+        },
+        { text: 'Status', value: 'status' },
+        { text: 'Rate', value: 'rate' },
+        { text: 'Time', value: 'created_at'}
+      ],
     }
   },
 
   mounted() {
     const url = 'http://localhost:3000/api/v1/movies'
-    axios
+    this.$axios
       .get(url)
       .then(response => (this.movies = response.data))
-  }
+  },
 }
 </script>
