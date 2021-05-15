44dfe4e4109da58b9bf0c60b766c23e41a54e12d
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-15 11:44:09 +0900

    json rspec

--- api/spec/requests/movies_spec.rb
+++ api/spec/requests/movies_spec.rb
@@ -28,12 +28,28 @@ RSpec.describe Api::V1::MoviesController, type: :request do
   end
 
   describe 'Post request' do
-    it 'Create new movie list' do
+    it 'Create new movie' do
       valid_params = { title: 'title', status: 'status', rate: 5 }
       # Verification to create data.
-      expect do
-        post api_v1_movies_url, params: { movie: valid_params }
-      end.to change(Movie, :count).by(1)
+      expect { post '/api/v1/movies', params: { movie: valid_params } }.to change(Movie, :count).by(+1)
+    end
+  end
+
+  describe 'Put request' do
+    it 'edit movie' do
+      movie = create(:movie, title: 'test-title')
+      put "/api/v1/movies/#{movie.id}", params: { movie: { title: 'new-title' } }
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json['title']).to eq('new-title')
+    end
+  end
+
+  describe 'Delete request' do
+    it 'delete movie' do
+      movie = create(:movie)
+      expect { delete "/api/v1/movies/#{movie.id}" }.to change(Movie, :count).by(-1)
+      expect(response.status).to eq(204)
     end
   end
 end
