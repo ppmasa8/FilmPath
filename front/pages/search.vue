07da3e0d9e6996e7600aeac80e4bf63a2c016519
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-23 07:52:00 +0900

    add TMDB apis

--- front/pages/search.vue
+++ front/pages/search.vue
@@ -1,19 +1,48 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <div class="search">
+    <h1>Search</h1>
+    <v-text-field
+      label="Search"
+      :rules="rules"
+      v-model='query'
+      hide-details="auto"
+      @keyup='getResult(query)'
+    ></v-text-field>
+    <div v-for='result in results' :key='result.id'>
+      <p>{{ result.title }}</p>
+      <img v-bind:src="'http://image.tmdb.org/t/p/w300/' + result.poster_path" width='100px'>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'search',
+  data () {
+    return {
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+      query: '',
+      results: ''
+    }
+  },
+  methods: {
+    getResult(query) {
+      if (!this.query) return;
+      const api_key = 'bca5abc8ed91fe4f233974561c897392'
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=' + query).then(response => {
+        this.results = response.data.results
+      });
+      console.log(this.results)
+    },
+
+
+    validate () {
+      this.$refs.form.validate()
+    },
+  }
+}
+</script>
