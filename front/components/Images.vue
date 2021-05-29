9f92d99dbf6629722d2302e4ba8bb7aa55c671af
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-30 02:26:20 +0900

    add noimage function

--- front/components/Images.vue
+++ front/components/Images.vue
@@ -3,8 +3,14 @@
     <v-card>
       <v-img
         :src="'http://image.tmdb.org/t/p/w300/' + results"
+        v-if="imageExists"
       >
       </v-img>
+      <v-img
+        v-else
+        src="https://1.bp.blogspot.com/-7DsADfq2BX4/Xlyf7aSybcI/AAAAAAABXq8/ut72jfLtCuo8ZvRGp1kqCYEbeQ0dOR8pgCNcBGAsYHQ/s1600/no_image_tate.jpg"
+      >
+      </v-img> 
     </v-card>
   </div>
 </template>
@@ -16,7 +22,8 @@ export default {
   props: ['Title'],
   data: () => {
     return {
-      results: [],
+      results    : [],
+      imageExists: false
     }
   },
 
@@ -24,6 +31,9 @@ export default {
     const API_KEY = 'bca5abc8ed91fe4f233974561c897392'
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${this.Title}`).then(res => {
       this.results = res.data.results[0].poster_path
+      if (this.results !== null) {
+        this.imageExists = true
+      }
     });
   }
 }
