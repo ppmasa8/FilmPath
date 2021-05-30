b3e199410177f015462f012c7aaab8bdabe9dd0e
ppmasa8 <kiyokiyopip56@gmail.com>
2021-05-31 01:21:26 +0900

    bug fixed

--- front/pages/Show.vue
+++ front/pages/Show.vue
@@ -1,4 +1,4 @@
-<template v-if="getShow">
+<template>
   <v-row justify="center">
     <v-dialog
       v-model="dialog"
@@ -23,24 +23,46 @@
             <v-img
               :src="'http://image.tmdb.org/t/p/w300/' + results.poster_path"
               max-width="400px"
+              v-if="movieExists"
+            >
+            </v-img>
+            <v-img
+              v-else
+              max-width="400px"
+              src="https://1.bp.blogspot.com/-7DsADfq2BX4/Xlyf7aSybcI/AAAAAAABXq8/ut72jfLtCuo8ZvRGp1kqCYEbeQ0dOR8pgCNcBGAsYHQ/s1600/no_image_tate.jpg"
             >
             </v-img>
           </v-col>
           <v-col cols="6">
-            <v-card-title class="mt-2">
+            <v-card-title class="mt-2" v-if="movieExists">
               {{ results.title }}
             </v-card-title>
-            <v-card-subtitle>
+            <v-card-title class="mt-2" v-else>
+              Undefined
+            </v-card-title>
+            <v-card-subtitle v-if="movieExists">
               Original title:  {{ results.original_title }}
             </v-card-subtitle>
-            <v-card-text>
+            <v-card-subtitle v-else>
+              Undefined
+            </v-card-subtitle>
+            <v-card-text v-if="movieExists">
               {{ results.overview }}
             </v-card-text>
-            <v-card-text>
+            <v-card-text v-else>
+              Undefined
+            </v-card-text>
+            <v-card-text v-if="movieExists">
               Release date:  {{ results.release_date }}
             </v-card-text>
-            <v-card-text>
+            <v-card-text v-else>
+              Release date:  Undefined
+            </v-card-text>
+            <v-card-text v-if="movieExists">
               Original language:  {{ results.original_language }}
+            </v-card-text>
+            <v-card-text v-else>
+              Original language:  Undefined
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
@@ -70,22 +92,20 @@ export default {
     return {
       results    : '',
       dialog     : false,
-      movieExists: true
+      movieExists: false
     }
   },
 
-  methods: {
-    getShow() {
+    mounted() {
       const API_KEY = 'bca5abc8ed91fe4f233974561c897392'
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${this.Title}`).then(res => {
         this.results = res.data.results[0]
-        if (this.results !== null) {
-          this.movieExists = false
+        if (this.results[0] !== null) {
+          this.movieExists = true
         }
-        return this.movieExists
+        return this.movieExists;
       });
     }
-  },
 }
 </script>
 
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -24,7 +24,6 @@
                 <v-card>
                   <Images v-bind:Title="movie.title"></Images>
 
-
                   <v-card-title class="subheading font-weight-bold">
                     {{ movie.title }}
                   </v-card-title>
